3bfbdfc910b3643618e62636b2768fe5a3fa9ac0
Patrick English <39728632+patrick-eng@users.noreply.github.com>
2019-06-06 13:12:19 +0100

    Updating set-up and details of examples for paper

--- GitUploads/Lagged DV Examples.R
+++ GitUploads/Lagged DV Examples.R
@@ -10,16 +10,22 @@ y1 <- c(1.3, 2.1, 3.3, 4.5, 5.4, 6.1, 6.8, 7.5, 8.1, 8.9, 9.6, 10.4, 11.5, 12.4,
 y2 <- c(1.1, 2.4, 3.4, 4.6, 5.7, 6.1, 8.2, 9.7, 10.2, 9.7, 10.1, 9.7, 10.1, 10.2, 10.5, 10.2, 10.3)
 y3 <- c(1.1, 2.4, 3.4, 4.6, 5.7, 6.1, 8.2, 9.7, 10.2, 9.7, 8.2, 6.1, 5.7, 4.6, 3.4, 2.4, 1.1)
 
+c1 <- arima.sim(model=list(ar=(round(runif(1, 0.1,0.99), digits=2)),
+                                 ma=(round(runif(1, 0.1,0.99), digits=2)),
+                                 order =c(1,0,1)), n=17, mean=2.5)
 
 t <- (0:16)
 
-data1 <- cbind(x, y1, t)
-data2 <- cbind(x, y2, t)
-data3 <- cbind(x, y3, t)
+data1 <- cbind(x, y1, c1, t)
+data2 <- cbind(x, y2, c1, t)
+data3 <- cbind(x, y3, c1, t)
 
 data1 <- as.data.frame(data1)
 data2 <- as.data.frame(data2)
 data3 <- as.data.frame(data3)
+
+
+data1$ly1 <- c(NA, data1$y[1:(length(data1$y)-1)])
 
 
 ### Standard Type II SS Anova
@@ -28,21 +34,20 @@ plot(y1, type="l", main="ANOVA Model");segments(x0=9, y0=0, y1=16, col="red", lt
 
 anovaplot <- grDevices::recordPlot()
 
-mod <- aov(lm(y1 ~ x, data=data1))
+mod <- aov(lm(y1 ~ x + ly1 + c1, data=data1))
 
-car::Anova(mod, type=2)
+summary(mod)
+
 
 
 ## Model on data 1
-itsa.model(data=data1, time="t", depvar="y1", interrupt_var = "x")
+model1 <- itsa.model(data=data1, time="t", depvar="y1", interrupt_var = "x", covariates = "c1", bootstrap = TRUE, no.plots = TRUE)
+
 
 
 ## Model on data 2
-itsa.model(data=data2, time="t", depvar="y2", interrupt_var = "x")
+model2 <- itsa.model(data=data2, time="t", depvar="y2", interrupt_var = "x")
 
 
 ## Model on data 3
 itsa.model(data=data3, time="t", depvar="y3", interrupt_var = "x")
-
-
-
--- GitUploads/Testing Immigration Opinions.R
+++ GitUploads/Testing Immigration Opinions.R
@@ -1,31 +1,35 @@
-##### British immigration opinions #####
+#### Testing the model on immigration data ####
 library(ggplot2)
+library(its.analysis)
 
-t.dat <- read.csv("/Users/patrickenglish/Documents/ITS 2019/publicopinioneexample_2.csv")
+
+## Read in
+t.dat <- read.csv("/Users/patrickenglish/Documents/ITS 2019/public-opinion-example.csv")
 
 head(t.dat)
 
-t.dat$public_opinion <- t.dat$public_opinion/100
-
-## plot interruption graph
-
-g <- ggplot(data=t.dat, aes(y=immigration, x=year))
-
-(graph <- g + geom_line())
-
-(graph <- graph + geom_segment(x=1997, y=100, xend=1997, yend=700, color="red") + 
-    geom_segment(x=2004, y=100, xend=2004, yend=700, color="red") + theme_bw()) 
-
-
-
-## run model (proper)
 t.dat$public_opinion2 <- scale(t.dat$public_opinion)
 
+### Plot the interruptions
+g <- ggplot(data=t.dat, aes(y=immigration, x=year))
 
-model <- itsa.model(data=t.dat, time="year", depvar="public_opinion2", interrupt_var = "interruption")
+(graph <- g + geom_line() +
+    xlab("") + ylab("Immigration (Thousands)") +
+    scale_x_continuous(breaks=seq(min(1980), max(2020), by=5)) + theme_bw())
 
+(graph <- graph + geom_segment(x=1997, y=100, xend=1997, yend=700, color="red") +
+    geom_segment(x=2006, y=100, xend=2006, yend=700, color="red"))
+
+
+
+## Run model (proper)
+
+
+model <- itsa.model(data=t.dat, time="year", depvar="public_opinion2",
+                    interrupt_var = "interruption")
 
 itsa.postest(model)
+
 
 
 ## run model (two-way)
@@ -38,11 +42,10 @@ itsa.postest(model)
 
 
 
-
-
 ## Check interaction - not significant, original model applies
 t.dat$lag_y <-c(NA, t.dat$public_opinion[1:(length(t.dat$public_opinion)-1)])
 
-t.dat$interact <- t.dat$interruption * t.dat$lag_y 
+t.dat$interact <- t.dat$interruption * t.dat$lag_y
 
 model <- itsa.model(data=t.dat, time="year", depvar="public_opinion", interrupt_var = "interruption_2", covariates = "interact")
+
