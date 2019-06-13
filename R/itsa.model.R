bb9fd7279727ac4658ffc586ea333a7be901cbc6
Patrick English <39728632+patrick-eng@users.noreply.github.com>
2019-06-13 12:49:28 +0100

    Major revisions as requested by CRAN volunteers
    Major updates as requested by CRAN volunteers upon upload.
    
    Also added are bootstrap functions, and its.analysis plot change from base R to gig-lot

--- R/itsa.model.R
+++ R/itsa.model.R
@@ -2,17 +2,18 @@
 #'
 #' Sets up an Interrupted Time Series Analysis (ITSA) for analysing short time series data.
 #'
-#' @param data define data
-#' @param time define time variable, must either be numeric (such as a year) or of class date
-#' @param depvar define dependent variable, must be continuous
-#' @param interrupt_var define interruption treatment/condition variable, must be a factor
-#' @param covariates specify a covariate, or vector of covariates, default is NULL
+#' @param data define data.
+#' @param time define time variable, must either be numeric (such as a year) or of class date.
+#' @param depvar define dependent variable, must be continuous.
+#' @param interrupt_var define interruption treatment/condition variable, must be a factor.
+#' @param covariates specify a covariate, or vector of covariates, default is NULL.
 #' @param alpha desired alpha (p-value boundary of null hypothesis rejection), default is 0.05.
-#' @param no.plots logical, specify whether function should return the ITSA plot, default is FALSE
-#' @return Summary object is created in the global environment named itsa.fit which contains results and necessary information for running post-estimation itsa.postest function. It also contains the Time Series Interruption plot (itsa.plot).
+#' @param no.plots logical, specify that function should not return the ITSA plot, default is FALSE.
+#' @param bootstrap logical, specify where function should run bootstrap estimations of F-values and return table of results, default is TRUE.
+#' @param Reps define number of replications for bootstrapping, default is 1000.
+#' @param print logical, specify whether the main model result should be printed to the console.
+#' @return returns a large list which can be assigned to the global environment, containing results and necessary information for running post-estimation itsa.postest function. It also contains the Time Series Interruption plot (itsa.plot) and bootstrap results, if applied.
 #' @export itsa.model
-#'
-#' @keywords time series, interrupted time series, analysis of variance
 #'
 #' @examples
 #'
@@ -29,35 +30,55 @@
 #' x <- as.data.frame(cbind(year, depv, interruption, cov1))
 #'
 #' # First example model
-#' itsa.model(data=x, time="year", depvar="depv", interrupt_var = "interruption", alpha=0.01)
-#'
-#' # Add a covariate
 #' itsa.model(data=x, time="year", depvar="depv", interrupt_var = "interruption",
-#' covariates = "cov1", alpha=0.1)
+#' alpha=0.05, bootstrap=TRUE, Reps = 250)
 #'
-#' # Example no significant result
-#' itsa.model(data=x, time="year", depvar="cov1", interrupt_var = "interruption", alpha=0.05)
+#' # Add a covariate, change alpha to 0.1
+#' itsa.model(data=x, time="year", depvar="depv", interrupt_var = "interruption",
+#' covariates = "cov1", alpha=0.1, bootstrap=TRUE, Reps = 250)
+#'
+#' # Force no plots and turn off bootstrap
+#' itsa.model(data=x, time="year", depvar="depv", interrupt_var = "interruption",
+#' covariates = "cov1", alpha=0.1, no.plots=TRUE, bootstrap=FALSE)
+#'
+#' # Example of no significant result
+#' itsa.model(data=x, time="year", depvar="cov1", interrupt_var = "interruption",
+#'  alpha=0.05, bootstrap=FALSE)
 #'
 #'
 #' @details This function provides a front door for the aov function in R's stats package, setting it up for running Interrupted Time Series Analysis (ITSA).
 #'
-#' Using the inputted variables, a Type-2 SS ANCOVA model is fitted which estimates the difference in means between interrupted and non-interrupted time periods, while accounting for the lag of the dependent variable and any further specified covariates.
+#' Using the inputted variables, a Type-2 Sum Squares ANCOVA Lagged Dependent Variable model is fitted which estimates the difference in means between interrupted and non-interrupted time periods, while accounting for the lag of the dependent variable and any further specified covariates.
 #'
-#' Variable names must be defined using quotation marks. Any further covariates can be passed into the covariates argument, either as a single vector or list of vectors (both must be within the same data as the dependent and independent variables).
+#' The function includes a bootstrap model by default, which runs 1000 replications of the main model with randomly drawn samples. A trimmed mean (10% removed) F-value is reported and a bootstrapped p-value derived from it. Users may turn off the bootstrapping model, or the number of replications for the bootstrap model to pass through can be altered using the Reps argument.
 #'
-#' Returns tables of time period group means (including counts and standard deviations), results from analysis of variance (including R-squared), and a summary of the result (relative to user defined alpha).
+#' Variable names must be defined using quotation marks. Any number of covariates can be passed into the covariates argument as a list of vectors (both must be within the same data as the dependent and independent variables).
 #'
-#' If there is suggestion of ANOVA assumption violation in the model, or evidence of residual autocorrelation, a warning message will appear. These tests and further post-estimation can be done through the itsa.postest function.
+#' Also returned as warning messages are the results of ANOVA and residual autocorrelation assumptions check ran in the background, if any are potentially violated. These tests and further post-estimation can be done through the itsa.postest function.
 #'
-#' If any of data, depvar, interrupt_var, or time are undefined, the function will stop and an error message will appear.
+#' Returns to console the results from analysis of variance test, results of the F-value bootstrap model, and a summary of the result (relative to user defined alpha) and any assumption violation warnings.
+#'
+#' Also returns a plot to the plot window graphing the dependent variable time series and interruption points. As this is a ggplot2 generated object, users can call the plot and make further customisations to it as an output.
+#'
+#' Assigning to an object will return a list of all of the above, plus further tests including a Tukey Honest Significant Differences test, the data and arguments set by the user, a table of time-series group means,, the full bootstrap model results and results of assumptions tests, and the full list of residual and fitted values.
+#'
+#' If any of data, depvar, interrupt_var, or time are undefined, the process will stop and an error message will appear.
 
 
 itsa.model <- function(data = NULL, time = NULL, depvar = NULL, interrupt_var = NULL,
-                       covariates = NULL, alpha = 0.05, no.plots=FALSE) {
+                       covariates = NULL, alpha = 0.05, no.plots=FALSE, bootstrap=TRUE, Reps=1000, print=TRUE) {
 
   ## Save global options and set new ones
-  default_ops <- options()
+  user.ops <- options()
+
   options(show.signif.stars = FALSE, contrasts = c("contr.sum","contr.poly"))
+
+  on.exit(options(user.ops))
+
+  #par.user <- graphics::par(no.readonly = TRUE)
+
+  #on.exit(graphics::par(par.user), add=TRUE)
+
 
 
   ## Check variable specifications
@@ -71,7 +92,7 @@ itsa.model <- function(data = NULL, time = NULL, depvar = NULL, interrupt_var = 
   }
 
   if(missing(interrupt_var)){
-    stop("Error: independent variable not defined", call.=TRUE)
+    stop("Error: interruption variable not defined", call.=TRUE)
   }
 
   if(missing(time)){
@@ -97,7 +118,7 @@ itsa.model <- function(data = NULL, time = NULL, depvar = NULL, interrupt_var = 
   }
 
 
-  ## Build object for means
+  ## Build object for means (point-estimates)
 
   ITSMeanValues <- plyr::ddply(x, ~interrupt_var,
                                plyr::summarise,count=length(depvar),
@@ -114,26 +135,31 @@ itsa.model <- function(data = NULL, time = NULL, depvar = NULL, interrupt_var = 
   if(no.plots==FALSE) {
 
     graphics::plot.new()
-    l <- graphics::legend(graphics::par('usr')[2], graphics::par('usr')[4], bty='n', xpd=NA,
-                          c("Response var", "Interruption"), col=c("black","dark grey"), lty=c(1,1), cex=0.5)
-    w <- graphics::grconvertX(l$rect$w, to='ndc') - graphics::grconvertX(0, to='ndc')
-    graphics::par(mar=c(5, 4, 4, 5), xpd=TRUE, omd=c(0, 1-w, 0, 1))
-    graphics::plot(data[,time], x$depvar, type = "l",
-                   xlab ="", ylab = "Response Variable Levels", col="black", main="Time Series Interruption Plot")
+
+    time <- data[,time]
+
+    temp <- as.data.frame(cbind(x, time))
+
+    g <- ggplot2::ggplot(data=temp, ggplot2::aes(y=depvar, x=time))
+
+    graph <- g + ggplot2::geom_line() +
+      ggplot2::xlab("") +  ggplot2::ylab("Response Variable Levels") +  ggplot2::theme_bw()
 
     for(i in periods){
-      graphics::segments(x0=i, y0=min(x$depvar), y1=max(x$depvar), col="red", lty=5)
+    graph <- graph +  ggplot2::geom_segment(x=i, y=min(temp$depvar), xend=i, yend=max(temp$depvar), color="red", linetype=8)
     }
 
-    graphics::legend(graphics::par('usr')[2], graphics::par('usr')[4], bty='n', xpd=NA,
-                     c("Response var", "Interruption"), col=c("black","red"), lty=c(1,5), cex=0.9)
+    graphics::plot(graph)
+
     itsa.plot <- grDevices::recordPlot()
 
   }
 
   else {
-    print('No plot forced')
+   itsa.plot <- "No plot forced"
   }
+
+
 
   ## Construct lag and shorten data
 
@@ -143,18 +169,87 @@ itsa.model <- function(data = NULL, time = NULL, depvar = NULL, interrupt_var = 
 
   x <- subset(x, stats::complete.cases(x))
 
+
+
   ## Build ANCOVA summary objects
 
   adjr_sq <- round(summary(stats::lm(data = x, depvar ~ .))$adj.r.squared, digits=4)
 
   model <- stats::aov(data = x, depvar ~ .)
 
-  ITSModResult <- car::Anova(model, type=2)
+  ITSModResult <- round(car::Anova(model, type=2), digits=4)
+
+  ITSModTukey <- stats::TukeyHSD(stats::aov(x$depvar ~ x$interrupt_var))
+
+  result <- ifelse(ITSModResult$`Pr(>F)`[1] < alpha,
+                   "Significant variation between time periods with chosen alpha",
+                   "No significant variation between time periods with chosen alpha")
+
+
+
+  ## Bootstrap F-values
+
+  if(bootstrap==TRUE){
+
+    set.seed(12345)
+
+    f.stat <- function(formula, data, indices) {
+      d <- data[indices,]
+      fit <- car::Anova(stats::aov(formula, data=d))
+      return(fit$`F value`)
+    }
+
+    f.stat.boot.results <- boot::boot(data=x, statistic=f.stat, R=Reps, formula=depvar ~ .)
+
+
+    booted.ints <- matrix(nrow=(ncol(f.stat.boot.results$t)-1), ncol=4)
+
+    for(i in 1:(ncol(f.stat.boot.results$t)-1)){
+
+      boots <- boot::boot.ci(f.stat.boot.results, type="perc", index=i)
+      booted.ints[i,2] <- boots$percent[,4]
+      booted.ints[i,3] <- mean(f.stat.boot.results$t[,i], na.rm=TRUE, trim = 0.1)
+      booted.ints[i,4] <- boots$percent[,5]
+
+    }
+
+    booted.ints <- as.data.frame(booted.ints)
+    names <- colnames(x)
+    names <- names[2:length(names)]
+    booted.ints$V1 <- names
+    names(booted.ints) <- c("Parameter", "Lower CI", "Mean F-value", "Upper CI")
+
+    booted.ints$`P-value` <- NA
+
+    for(i in 1:nrow(booted.ints)){
+      booted.ints[i,5] <- stats::pf(booted.ints[i,3], df1= ITSModResult[i,2], df2=ITSModResult["Residuals",2], lower.tail = FALSE)
+    }
+
+
+  }
+
+  else {
+
+    f.stat.boot.results <- "No bootstrapping"
+
+  }
+
+
+
+  ## Build assumption test summary objects
 
   stest <- stats::shapiro.test(model$residuals)
+
   stest_r <- round(stest[["p.value"]], digits=4)
 
+  ltest <- car::leveneTest(x$depvar ~ x$interrupt_var)
+
+  ltest_r <- round((ltest[1,3]), digits=4)
+
   acf.test <- forecast::taperedacf(model$residuals, lag.max = 5, plot=FALSE)
+
+
+
 
   acf.data <- cbind(acf.test$z, acf.test$upper, acf.test$lower)
 
@@ -163,67 +258,61 @@ itsa.model <- function(data = NULL, time = NULL, depvar = NULL, interrupt_var = 
   acf.out <- ifelse("FAIL" %in% acf.results[1:3,1], "Evidence of autocorrelation", "No autocorrelation evidence")
 
 
-  ltest <- car::leveneTest(x$depvar ~ x$interrupt_var)
-  ltest_r <- round((ltest[1,3]), digits=4)
-
-
-
-  result <- ifelse(ITSModResult$`Pr(>F)`[1] < alpha,
-                   "Significant variation between time periods with chosen alpha",
-                   "No significant variation between time periods with chosen alpha")
 
   post_sums <- ifelse((stest_r < 0.2 | ltest_r < 0.15 | acf.out == "Evidence of autocorrelation"),
                       "Warning: ANCOVA Result may be biased. Please check post-estimation.",
                       "")
 
-
-
   post_sums_s <- ifelse((stest_r < 0.2 | ltest_r < 0.15 | acf.out == "Evidence of autocorrelation"),
                         "Post-Est Warning",
                         "No Post-Est Warning")
 
+
+
   ## Build object for summary
 
-  itsa.fit <<- as.list("ITSA Model Fit")
-  itsa.fit$aov.result <<- ITSModResult
-  itsa.fit$alpha <<- alpha
-  itsa.fit$itsa.result <<- result
-  itsa.fit$group.means <<- ITSMeanValues
-  itsa.fit$dependent <<- x$depvar
-  itsa.fit$interrupt_var <<- x$interrupt_var
-  itsa.fit$residuals <<- model$residuals
-  itsa.fit$fitted.values <<- model$fitted.values
-  itsa.fit$shapiro.test <<- stest_r
-  itsa.fit$levenes.test <<- ltest_r
-  itsa.fit$autcorr <<- acf.out
-  itsa.fit$post_sums <<- post_sums_s
-  itsa.fit$adjr_sq <<- adjr_sq
+  itsa.fit <- as.list("ITSA Model Fit")
+  itsa.fit$aov.result <- ITSModResult
+  itsa.fit$tukey.result <- ITSModTukey
+  itsa.fit$data <- x
+  itsa.fit$alpha <- alpha
+  itsa.fit$itsa.result <- result
+  itsa.fit$group.means <- ITSMeanValues
+  itsa.fit$dependent <- x$depvar
+  itsa.fit$interrupt_var <- x$interrupt_var
+  itsa.fit$residuals <- model$residuals
+  itsa.fit$fitted.values <- model$fitted.values
+  itsa.fit$shapiro.test <- stest_r
+  itsa.fit$levenes.test <- ltest_r
+  itsa.fit$autcorr <- acf.out
+  itsa.fit$post_sums <- post_sums_s
+  itsa.fit$adjr_sq <- adjr_sq
+  itsa.fit$fstat.bootstrap <- f.stat.boot.results
+  itsa.fit$itsa.plot <- itsa.plot
 
-  if(no.plots==FALSE) {
-
-    itsa.fit$itsa.plot <<- itsa.plot
-
+  if(bootstrap==TRUE){
+    itsa.fit$booted.ints <- booted.ints
   }
 
 
+  ## Return model result objects
+  if(print == TRUE){
+      print(ITSModResult)
+      cat(paste('', '\n'))
 
-  ## Return objects
-  cat(paste('', '\n'))
-  cat(paste('Mean Values of Dependent Variable Between Time Periods:', '\n'))
-  print(ITSMeanValues)
-  cat(paste('', '\n', '\n'))
-  cat(paste('Analysis of Variances:', '\n'))
-  print(ITSModResult)
-  cat(paste('', '\n'))
-  cat(paste('Model Adjusted R-Squared:', adjr_sq, '\n'))
-  cat(paste('', '\n'))
-  cat(paste('Result: ', result, ' (<', alpha,')', '\n', sep =""))
-  cat(paste('', '\n'))
-  cat(post_sums)
-  cat(paste('', '\n'))
+      if(bootstrap==TRUE){
+        cat(paste('Bootstrapped F-values:', '\n'))
+        print(itsa.fit$booted.ints[1,c(2:5)], digits=5, row.names=FALSE)
 
-  ## reset global options
-  options(default_ops)
-  graphics::par(mar=c(5.1, 4.1, 4.1, 2.1), omd=c(0,1,0,1), xpd=FALSE)
+      }
 
+      message(cat(paste('', '\n', 'Result: ', result, ' (<', alpha,')', '\n', sep ="")))
+
+  }
+
+  if(itsa.fit$post_sums=="Post-Est Warning"){
+   warning("ANCOVA Result may be biased. Please check post-estimation.", call.=FALSE)
+  }
+
+  return(itsa.fit)
 }
--- R/itsa.postest.R
+++ R/itsa.postest.R
@@ -2,11 +2,12 @@
 #'
 #' Function to produce post-estimation plots from the itsa.model function.
 #'
-#' @param model define ITSA model results object, default is output from itsa.model function
-#' @param no.plots logical, specify whether function should present post-estimation plots (default is FALSE)
-#' @param alpha define alpha level for test statistics, default is 0.05
-#' @return if no.plots is not forced, will return a table to the console and two plots to the plot tab
-#' @keywords time series, interrupted time series, analysis of variance, post-estimation
+#' @param model define ITSA model results object.
+#' @param no.plots logical, specify whether function should present post-estimation plots (default is FALSE).
+#' @param alpha define alpha level for test statistics, default is 0.05.
+#' @param bootstrap logical, declare if itsa.model was bootstrapped for plot of these results.
+#' @param print logical, specify whether the main model result should be printed to the console.
+#' @return returns a list containing post-estimation test results and four plots (plus one additional if bootstrapping applied) which are also sent directly to the plot tab.
 #' @export itsa.postest
 #'
 #' @details This function runs and reports post-estimation tests on fits from the itsa.model function, and generates four plots.
@@ -19,57 +20,146 @@
 #'
 #' A QQ-Norm and Boxplot are generated with the test results overlaid (respectively), with a Residual v Fitted and Autocorrelation Function Plot also generated.
 #'
+#' The results of bootstrap estimations in itsa.model will be plotted, unless argument is switched to FALSE.
+#'
 #' Default is to generate plots and summary table, but plots may be overriden using no.plots argument.
 #'
 #' Default alpha value for post-estimation statistics is 0.05, test results will suggest potential presence of problems at higher values (and also at higher levels relative to a user-inputted alpha), but user discretion is needed (examined in tandam with the Residuals v Fitted plot).
 #'
 #' See 'itsa.model' documentation for further information.
 
-itsa.postest <- function(model = NULL, no.plots = FALSE, alpha = 0.05) {
+itsa.postest <- function(model = NULL, no.plots = FALSE, alpha = 0.05, bootstrap = TRUE, print = TRUE) {
 
   if(missing(model)) {
-    model <- itsa.fit
+    stop("Error: model object not defined", call.=TRUE)
   }
 
-  else {
-    model <- model
-  }
+
+  user.ops <- options()
+
+  options(show.signif.stars = FALSE, contrasts = c("contr.sum","contr.poly"))
+
+  on.exit(options(user.ops))
 
 
   if(no.plots==FALSE) {
 
     ### Plots
+    graphics::plot.new()
+
+    ## Bootstrap plot
+    if(bootstrap==TRUE){
+
+      graphics::plot(model$fstat.bootstrap, index=1)
+      bootstrap.plot <- grDevices::recordPlot()
+
+      }
 
 
     # Result text objects
     stest_ob <- paste("Shapiro-Wilk Test of Abnormality: p =", model$shapiro.test)
     ltest_ob <- paste("Levene's Test of Heterogeneous Variances: p =", model$levenes.test)
 
+
     # Residual v Fitted
     graphics::plot(model$fitted.values, model$residuals,
                    main="ITSA Residuals v Fitted Plot",
                    xlab="ITSA Fitted Values",
                    ylab="ITSA Residuals");graphics::abline(0,0, col="red")
+    residual.plot <- grDevices::recordPlot()
+
 
     # ACF plot
     forecast::Acf(model$residuals, main="ITSA Autocorrelation Plot")
+    acf.plot <- grDevices::recordPlot()
+
 
     # DV v Factor Boxplot
     graphics::boxplot(model$dependent ~ model$interrupt_var,
                       main="Group Variances",
                       xlab="Time Period",
-                      ylab="Dependent Variable")
-    graphics::mtext(ltest_ob, side=3)
+                      ylab="Dependent Variable");graphics::mtext(ltest_ob, side=3)
+    variance.plot <- grDevices::recordPlot()
 
 
     # QQ-Norm plot
     stats::qqnorm(model$residuals,
                   main="ITSA QQ-Norm Plot");stats::qqline(model$residuals, col="red")
     graphics::mtext(stest_ob, side=3)
+    qqnorm.plot <- grDevices::recordPlot()
 
 
 
     ## Summary Table
+    stest <- model$shapiro.test
+    ltest <- model$levenes.test
+
+    if(stest < alpha){
+      stest_r <- "Non-normality present"
+    }
+    else{
+      if(stest < alpha * 4){
+        stest_r <- "Non-normality could be present"
+      }
+
+      else{
+        stest_r <- "Non-normality not present"
+      }
+    }
+
+
+    if(ltest < alpha){
+      ltest_r <- "Heterogeneous variances present"
+    }
+    else{
+      if(ltest < alpha * 2){
+        ltest_r <- "Heterogeneous variances could be present"
+      }
+      else{
+        ltest_r <- "Heterogeneous variances not present"
+      }
+    }
+
+
+
+    acftest <- model$autcorr
+    acftest_r <- ifelse(acftest=="Evidence of autocorrelation", "Autocorrelation may be present", "Autocorrelation not present")
+
+
+
+    x <- c("Shapiro-Wilk", "Levene's", "Autocorrelation")
+    p <- c(stest, ltest, "NA")
+    y <- c(stest_r, ltest_r, acftest_r)
+
+    z <- as.data.frame(cbind(x, p, y))
+
+    colnames(z) <- c("Test", "P-value", "Result")
+
+    itsa.postest.results <- as.list("ITSA Post-Estimation")
+    itsa.postest.results$test_results <- z
+    if(bootstrap==TRUE){
+      itsa.postest.results$bootstrap_plot <- bootstrap.plot
+    }
+    itsa.postest.results$residual_plot <- residual.plot
+    itsa.postest.results$acf_plot <- acf.plot
+    itsa.postest.results$variance_plot <- variance.plot
+    itsa.postest.results$qqnorm_plot <- qqnorm.plot
+
+    if(print==TRUE){
+      z
+    }
+
+    return(itsa.postest.results)
+
+  }
+
+
+
+
+  else {
+
+    ## Summary Table
+
 
     stest <- model$shapiro.test
     ltest <- model$levenes.test
@@ -101,40 +191,26 @@ itsa.postest <- function(model = NULL, no.plots = FALSE, alpha = 0.05) {
       }
     }
 
+    acftest <- model$autcorr
+    acftest_r <- ifelse(acftest=="Evidence of autocorrelation", "Autocorrelation may be present", "Autocorrelation not present")
 
-    x <- c("Shapiro-Wilk", "Levene's")
-    p <- c(stest, ltest)
-    y <- c(stest_r, ltest_r)
-
-    z <- as.data.frame(cbind(x, p, y))
-
-    colnames(z) <- c("Test", "P-value", "Result")
-
-    cat(paste("Residual Post-Estimation Test Results", '\n', '\n'))
-    z
-
-  }
-
-  else {
-
-    ## Summary Table
-
-    stest <- model$shapiro.test
-    ltest <- model$levenes.test
-
-    stest_r <- ifelse(stest < alpha, "Non-normality present", "Non-normality not present")
-    ltest_r <- ifelse(ltest < alpha, "Heterogeneous variances present", "Heterogeneous variances not present")
-
-    x <- c("Shapiro-Wilk", "Levene's")
-    p <- c(stest, ltest)
-    y <- c(stest_r, ltest_r)
+    x <- c("Shapiro-Wilk", "Levene's", "Autocorrelation")
+    p <- c(stest, ltest, "NA")
+    y <- c(stest_r, ltest_r, acftest_r)
 
     z <- as.data.frame(cbind(x, p, y))
 
     colnames(z) <- c("Test", "P-value", "Result")
 
-    cat(paste("Residual Post-Estimation Test Results", '\n', '\n'))
-    z
+    itsa.postest.results <- as.list("ITSA Post-Estimation")
+    itsa.postest.results$test_results <- z
+    itsa.postest.results$plots <- "No plots forced"
+
+    if(print==TRUE){
+      z
+    }
+
+    return(itsa.postest.results)
 
   }
 
